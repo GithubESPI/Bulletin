49d06d04a7279dc8c6d13dce44fa9777ee2f2196
GithubESPI <165779233+GithubESPI@users.noreply.github.com>
2024-08-26 08:49:10 +0200

    Update ButtonsProvider.tsx

--- src/components/ButtonsProvider.tsx
+++ src/components/ButtonsProvider.tsx
@@ -4,10 +4,10 @@ import { Button } from "./ui/button";
 const ButtonsProvider = () => {
   return (
     <div className="flex flex-col space-y-4">
-      {/* <Button onClick={() => signIn("azure-ad")} className="bg-primary-50 p-6">Se connecter avec Azure</Button> */}
-      <Button onClick={() => signIn("github")} className="bg-primary-50 p-6">
+      <Button onClick={() => signIn("azure-ad")} className="bg-primary-50 p-6">Se connecter avec mon compte ESPI</Button>
+      {/* <Button onClick={() => signIn("github")} className="bg-primary-50 p-6">
         Se connecter avec mon compte ESPI
-      </Button>
+      </Button> */}
     </div>
   );
 };
