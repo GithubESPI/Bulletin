a5dd745ad085a876ba3fb6cbbdd2eebfcf5e20e1
GithubESPI <a.vespuce@groupe-espi.fr>
2024-09-06 16:35:54 +0200

    Finalisation du frontend

--- src/components/ButtonsProvider.tsx
+++ src/components/ButtonsProvider.tsx
@@ -15,34 +15,32 @@ const ButtonsProvider = () => {
       setLoading(false); // Si une erreur se produit, arrêtez le loader
     }
   };
+  /* <Button onClick={() => signIn("azure-ad")} className="bg-primary-50 p-6">Se connecter avec Azure</Button> */
 
   return (
-    <div className="flex flex-col space-y-4">
-<<<<<<< HEAD
-      <Button
-        onClick={handleSignIn}
-        className="w-full bg-primary-50 p-6 flex items-center justify-center"
-        disabled={loading} // Désactiver le bouton pendant le chargement
-      >
-        {loading ? (
-          <FaSpinner className="h-4 w-4 mr-2 animate-spin" /> // Icône de spinner pour le chargement
-        ) : (
-          <SiMicrosoftazure className="h-4 w-4 mr-2" />
-        )}
-        <p className="text-white">Se connecter avec mon compte ESPI</p>
-      </Button>
-=======
-      <Button onClick={() => signIn("azure-ad")} className="bg-primary-50 p-6">Se connecter avec mon compte ESPI</Button>
-      {/* <Button onClick={() => signIn("github")} className="bg-primary-50 p-6">
+    <>
+      <div className="flex flex-col space-y-4">
+        <Button
+          onClick={handleSignIn}
+          className="w-full bg-primary-50 p-6 flex items-center justify-center"
+          disabled={loading} // Désactiver le bouton pendant le chargement
+        >
+          {loading ? (
+            <FaSpinner className="h-4 w-4 mr-2 animate-spin" /> // Icône de spinner pour le chargement
+          ) : (
+            <SiMicrosoftazure className="h-4 w-4 mr-2" />
+          )}
+          <p className="text-white">Se connecter avec mon compte ESPI</p>
+        </Button>
+        <Button onClick={() => signIn("azure-ad")} className="bg-primary-50 p-6">
+          Se connecter avec mon compte ESPI
+        </Button>
+        {/* <Button onClick={() => signIn("github")} className="bg-primary-50 p-6">
         Se connecter avec mon compte ESPI
       </Button> */}
->>>>>>> 49d06d04a7279dc8c6d13dce44fa9777ee2f2196
-    </div>
+      </div>
+    </>
   );
 };
 
 export default ButtonsProvider;
-
-{
-  /* <Button onClick={() => signIn("azure-ad")} className="bg-primary-50 p-6">Se connecter avec Azure</Button> */
-}
