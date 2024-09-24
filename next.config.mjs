16f5be9b8e6de59dd27bca612f0a882b60980bb8
GithubESPI <a.vespuce@groupe-espi.fr>
2024-09-24 13:29:42 +0200

    Push

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,14 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    const urlString = process.env.NEXTAUTH_URL || ""; // Valeur par défaut vide si NEXTAUTH_URL est manquante
-    if (urlString.trim() === "") {
-      console.error("Invalid NEXTAUTH_URL: ", urlString);
-      return []; // Retourne une configuration vide pour éviter de bloquer le build
-    }
-
-    try {
-      new URL(urlString);
-      // Si l'URL est valide, continuer
-      return [
-        {
-          source: "/old-path",
-          destination: "/new-path",
-          permanent: true,
-        },
-      ];
-    } catch (error) {
-      console.error("Error with NEXTAUTH_URL: ", error);
-      return []; // Retourne une configuration vide pour éviter de bloquer le build
-    }
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https", // Spécifiez le protocole (http ou https)
+        hostname: "assets.dewatermark.ai", // Le nom de domaine des images externes
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
