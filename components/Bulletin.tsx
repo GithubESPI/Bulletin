3dc9417c278abfd098b774b29bcc7ed64548b68d
GithubESPI <a.vespuce@groupe-espi.fr>
2024-09-24 09:52:25 +0200

    Mise à jour du .env

--- components/Bulletin.tsx
+++ components/Bulletin.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import { HTMLAttributes } from "react";
 
 interface BulletinProps extends HTMLAttributes<HTMLDivElement> {
@@ -9,13 +10,13 @@ interface BulletinProps extends HTMLAttributes<HTMLDivElement> {
 const Bulletin = ({ imgSrc, className, dark = false, ...props }: BulletinProps) => {
   return (
     <div className={cn("relative pointer-events-none z-50 overflow-hidden", className)} {...props}>
-      <img
+      <Image
         src={dark ? "/bulletin.png" : "/bulletin.png"}
         className="pointer-events-none z-50 select-none"
         alt="phone image"
       />
       <div className="absolute -z-10 inset-0">
-        <img
+        <Image
           className="object-cover min-w-full min-h-full"
           src={imgSrc}
           alt="overlaying phone image"
